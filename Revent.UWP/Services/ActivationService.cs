5e55f10ec9dd81d2c37eda8448ee75654068986f
Rick D <ikarago@outlook.com>
2019-06-03 20:25:45 +0200

    More UI changes

--- Revent.UWP/Services/ActivationService.cs
+++ Revent.UWP/Services/ActivationService.cs
@@ -7,7 +7,10 @@ using Revent.UWP.Activation;
 using Revent.UWP.Core.Helpers;
 
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
 using Windows.System;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
@@ -59,6 +62,10 @@ namespace Revent.UWP.Services
 
             if (IsInteractive(activationArgs))
             {
+                // Set custom stuff
+                SetTitlebar();
+                SetMinimalWindowSize();
+
                 // Ensure the current window is active
                 Window.Current.Activate();
 
@@ -128,6 +135,32 @@ namespace Revent.UWP.Services
         private bool IsInteractive(object args)
         {
             return args is IActivatedEventArgs;
+        }
+
+        private void SetTitlebar()
+        {
+            // Make the buttons transparent
+            ApplicationViewTitleBar titlebar = ApplicationView.GetForCurrentView().TitleBar;
+            titlebar.ButtonBackgroundColor = Colors.Transparent;
+            titlebar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            if (App.Current.RequestedTheme == ApplicationTheme.Dark)
+            {
+                titlebar.ButtonForegroundColor = Colors.White;
+            }
+            else if (App.Current.RequestedTheme == ApplicationTheme.Light)
+            {
+                titlebar.ButtonForegroundColor = Colors.Black;
+            }
+
+            // Extend the normal window to the Titlebar for the blur to reach there too
+            CoreApplicationViewTitleBar coreTitlebar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitlebar.ExtendViewIntoTitleBar = true;
+        }
+
+        private void SetMinimalWindowSize()
+        {
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Windows.Foundation.Size(360, 300));
         }
     }
 }
